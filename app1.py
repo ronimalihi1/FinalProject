54821755f89d1e7ce0d3f41f16627ba704207486
ronimalihi1 <62188356+ronimalihi1@users.noreply.github.com>
2021-10-21 22:36:54 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -378,32 +378,8 @@ def main():
                        # df3['District']('Haifa').value_counts()
                         #st.markdown(" TEST")
                         # Layout Templates
-                        html_temp = """
-                        <div style="background-color:{};padding:10px;border-radius:10px">
-                        <h1 style="color:{};text-align:center;">Simple Blog </h1>
-                        </div>
-                        """
-                        title_temp ="""
-                        <div style="background-color:#464e5f;padding:10px;border-radius:10px;margin:10px;">
-                        <h4 style="color:white;text-align:center;">{}</h1>
-                        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;float:left;width: 50px;height: 50px;border-radius: 50%;" >
-                        <h6>District:{}</h6>
-                        <br/>
-                        <br/> 
-                        <p style="text-align:justify">{}</p>
-                        </div>
-                        """
-                        article_temp ="""
-                        <div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
-                        <h4 style="color:white;text-align:center;">{}</h1>
-                        <h6>Author:{}</h6> 
-                        <h6>Post Date: {}</h6>
-                        <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;width: 50px;height: 50px;border-radius: 50%;" >
-                        <br/>
-                        <br/>
-                        <p style="text-align:justify">{}</p>
-                        </div>
-                        """
+                    
+                 
                         head_message_temp ="""
                         <div style="background-color:#464e5f;padding:10px;border-radius:5px;margin:10px;">
                         <h4 style="color:white;text-align:center;"> Custumer ID: {} </h1>
@@ -415,11 +391,7 @@ def main():
                         </div>
                         
                         """
-                        full_message_temp ="""
-                        <div style="background-color:silver;overflow-x: auto; padding:10px;border-radius:5px;margin:10px;">
-                        <p style="text-align:justify;color:black;padding:10px">{}</p>
-                        </div>
-                        """
+                     
                         result = view_all_customers()
                       #  st.write(t)
                        # dff= pd.DataFrame(result,columns=["user_id","Gender","Age","District"])
@@ -512,6 +484,11 @@ def main():
                             
                         aisle_id=st.text_input("aisle ID")
                         department_id=st.text_input("department id")
+			product_exist=get_product(product_id)
+			if product_exsit:
+                          st.warning("You Can't Use This USERNAME, Please choose another one ")
+
+				
                      
                     
                     #cosine_sim= vectorize_text(df['aisle'])  
@@ -519,9 +496,9 @@ def main():
                     #num_of_rec= st.sidebar.number_input("Number",4,20,7)
                         
 
-                        if st.button("Add Product"):
+                        elif st.button("Add Product"):
                             
-                        
+                           
                             add_data(product_id,product_name,aisle_id,department_id)
                             st.success("Product ID: {} Added  To Products".format(product_id))
                             with st.expander("Read"):
