74356468b24b0581fc78b5078bb929ec6c609d03
ronimalihi1 <62188356+ronimalihi1@users.noreply.github.com>
2021-10-21 21:26:57 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -168,7 +168,7 @@ def view_all_managers():
 #from sklearn.feature_extraction.text import CountVectorizer
 #from sklearn.metrics.pairwise import cosine_similarity,linear_kernel
 def get_user(user_id):
-    c7.execute('SELECT * FROM userstable WHERE username="{}"'.format(user_id))
+    c7.execute('SELECT * FROM userstable WHERE user_id="{}"'.format(user_id))
     data = c7.fetchall()
     return data
 def load_data(data):
