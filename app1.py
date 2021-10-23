13afcd19cba7dc44c4e50d34e92d0d56c3a30d6c
ronimalihi1 <62188356+ronimalihi1@users.noreply.github.com>
2021-10-23 13:26:54 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -408,7 +408,7 @@ def main():
                             #
                             
                         st.subheader("Search Customer")
-			cus_df=view_all_customers()
+                        cus_df=view_all_customers()
                         df=pd.DataFrame(cus_df,columns=['user_id','Gender','Age','District'])
                         with st.expander("View All Users"):
                             st.dataframe(df)
